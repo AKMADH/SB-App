c9741fff04b0b733100cd19cbeb88f386818ae4c
Anish kumar Mishra <akmadh2013@gmail.com>
2020-08-21 01:04:55 +0530

    Added routing related to hotel

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
+import { UpdateHotelComponent } from './hotel/update-hotel/update-hotel.component';
+import { DeleteHotelComponent } from './hotel/delete-hotel/delete-hotel.component';
+import { HotelviewComponent } from './hotel/hotelview/hotelview.component';
+import { AddhotelComponent } from './hotel/addhotel/addhotel.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HotelDetailsComponent } from './hotel/hotel-details/hotel-details.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:DashboardComponent},
+  { path: 'Addhotel', component: AddhotelComponent },
+  { path: 'hotel-detail/:id', component:  HotelDetailsComponent},
+  { path: 'hotelview', component:  HotelviewComponent},
+  { path: 'delete:id', component:  DeleteHotelComponent},
+  { path: 'update/:id', component:  UpdateHotelComponent},
+  { path: '', component:  DashboardComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,52 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {CommonModule} from "@angular/common"
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TopnavComponent } from './common/topnav/topnav.component';
+import { FooterComponent } from './common/footer/footer.component';
+import { HttpClientModule} from '@angular/common/http';
+import { AngularEditorModule } from '@kolkov/angular-editor';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AddhotelComponent } from './hotel/addhotel/addhotel.component';
+import { HotelDetailsComponent } from './hotel/hotel-details/hotel-details.component';
+import { HotelviewComponent } from './hotel/hotelview/hotelview.component';
+import { UpdateHotelComponent } from './hotel/update-hotel/update-hotel.component';
+import { DeleteHotelComponent } from './hotel/delete-hotel/delete-hotel.component';
+import { AngularFireModule } from "@angular/fire";
+import { environment } from "../environments/environment";
+import { AngularFireStorage } from '@angular/fire/storage';
+import {
+  AngularFireStorageModule
 
+} from "@angular/fire/storage";
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    TopnavComponent,
+    FooterComponent,
+    AddhotelComponent,
+    HotelDetailsComponent,
+    HotelviewComponent,
+    UpdateHotelComponent,
+    DeleteHotelComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    AngularEditorModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule,
+    AngularFireStorageModule,
+     AngularFireModule,
+     AngularFireModule.initializeApp(environment.firebaseConfig, "cloud")
   ],
-  providers: [],
+  providers: [AngularFireStorage],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
